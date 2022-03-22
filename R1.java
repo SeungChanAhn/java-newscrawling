786360793242979c715f913b39901c9c732397ea
SeungChanAhn <102018797+SeungChanAhn@users.noreply.github.com>
2022-03-22 15:28:04 +0900

    20220321_Review1_1
    1번 문제

--- R1.java
+++ R1.java
@@ -1,30 +1,42 @@
-package Prac0322;
+package Prac0321;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class R1 {
 
 	public static void main(String[] args) {
-		while (true) {
-			Scanner scanner = new Scanner(System.in);
-			String first = scanner.nextLine();
-			String second = scanner.nextLine();
-			String third = scanner.nextLine();
-			
-			// 한 행이 0000이거나, 열이 000 이면 Pass, 아니면 Fail. ASCII코드 48은 0이다.
-			if (first == "0000" && second == "0000" && third == "0000") {
-				System.out.println("Pass");
-			} else if (first.charAt(0) == 48 && second.charAt(0) == 48 && third.charAt(0) == 48) {
-				System.out.println("Pass");
-			} else if (first.charAt(1) == 48 && second.charAt(1) == 48 && third.charAt(1) == 48) {
-				System.out.println("Pass");
-			} else if (first.charAt(2) == 48 && second.charAt(2) == 48 && third.charAt(2) == 48) {
-				System.out.println("Pass");
-			} else if (first.charAt(3) == 48 && second.charAt(3) == 48 && third.charAt(3) == 48) {
-				System.out.println("Pass");	
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String line1 = scanner.nextLine();
+		String line2 = scanner.nextLine();
+		List<Character> list1 = new ArrayList<>();
+		List<Character> list2 = new ArrayList<>();
+		////////////////////////////////////////////// 각 문자를 리스트에 오름차순정렬
+		char[] word1 = new char[line1.length()];
+		for (int i = 0; i < line1.length(); i++) {
+			word1[i] = line1.charAt(i);
+			list1.add(word1[i]);
+		}
+		Collections.sort(list1);
+
+		char[] word2 = new char[line2.length()];
+		for (int i = 0; i < line2.length(); i++) {
+			word2[i] = line2.charAt(i);
+			list2.add(word2[i]);
+		}
+		Collections.sort(list2);
+		////////////////////////////////////////////// 리스트 비교
+		if (list1.size() == list2.size()) {
+			if (list1.containsAll(list2) == true) {
+				System.out.println("YES");
 			} else {
-				System.out.println("Fail");
+				System.out.println("NO");
 			}
+		} else {
+			System.out.println("NO");
 		}
 	}
 }
